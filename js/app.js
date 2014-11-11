113da7cdbb418b6c97d944555314a6c3175c111c
volodymyr_kravchyk <volodymyr.kravchyk@lezgro.com>
2014-11-11 18:37:40 +0200

    deleting all files

--- js/app.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    var app = angular.module('blog', []);
-    app.controller('IncludeController',function(){
-        this.header ='html/header.html';
-        this.aside ='html/aside.html';
-    });
-})();
