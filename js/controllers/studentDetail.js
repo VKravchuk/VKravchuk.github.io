8481936698c2d61d8926970855f0259008fe1855
VKravchuk <VKravchuk94@gmail.com>
2014-12-07 12:56:26 +0200

    add json for students

--- js/controllers/studentDetail.js
+++ js/controllers/studentDetail.js
@@ -1,6 +1,7 @@
 studentApp.controller('StudentDetailCtrl', ['$scope', '$routeParams', '$http',
 	function($scope, $routeParams, $http){
-		$http.$.get('json/' + routeParams.studentId + '.json').success(function(data) {
+		$http.get('json/students/' + $routeParams.studentId + '.json').success(function(data) {
 			$scope.student = data;
-		/});
+			console.log($scope.student.points[0].point);
+		});
 	}]);
